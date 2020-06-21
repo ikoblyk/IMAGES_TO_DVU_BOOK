990457f6f6b4d08b107fc1103d5ba86e0038f37d
ivan <ivankob.16@gmail.com>
2020-06-21 15:53:34 +0300

    python_version_3.5

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
-FROM robcherry:docker-chromedriver
-
+FROM robcherry/docker-chromedriver:latest
+RUN mkdir -p /home/automation/app && chown -R automation:automation /home/automation/app &&  apt-get update
+#RUN  apt-get install -y make build-essential libssl-dev zlib1g-dev &&  apt-get install -y libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm &&  apt-get install -y libncurses5-dev  libncursesw5-dev xz-utils tk-dev && wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz && tar xvf Python-3.6.4.tgz && cd Python-3.6.4 && ./configure --enable-optimizations && make -j8 &&  make altinstall
--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="BOOK",
-    version="0.0.1",
+    name="bookhack_ikoblyk",
+    version="0.0.3",
     author="Ivan Koblyk",
     author_email="ivankob.16@gmail.com",
     description="Book downloading tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ikoblyk/IMAGES_TO_DVU_BOOK.git ",
+    url="https://github.com/ikoblyk/IMAGES_TO_DVU_BOOK.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     install_requires = [
         'Cython==0.29.14',
         'selenium==3.141.0',
